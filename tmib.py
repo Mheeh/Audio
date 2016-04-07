8781c9972e71809fc6e2e381288336c5df79e1c1
Marcin Lipa <marcin.lipa@protonmail.com>
2016-04-07 16:07:16 +0200

    Dodawanie plików do biblioteki muzycznej

--- tmib.py
+++ tmib.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from werkzeug import utils
 import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = app.static_folder + '/music'
+ALLOWED_EXTENSIONS = set(['wav'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
 
-@app.route('/play')
-@app.route('/play/<music>')
+@app.route('/play', methods=['GET'])
+@app.route('/play/<music>', methods=['GET'])
 def play(music=None):
     return render_template('play.html', music=music)
 
 
-@app.route('/library')
+@app.route('/library', methods=['GET', 'POST'])
 def library():
-    library = os.listdir(app.static_folder + '/music')
+    if request.method == 'POST':
+        file = request.files['music']
+        if file and allowed_file(file.filename):
+            filename = utils.secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        return redirect(url_for('library'))
+
+    library = os.listdir(app.config['UPLOAD_FOLDER'])
     library.sort()
 
     return render_template('library.html', library=library)
