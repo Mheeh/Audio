1cf395f14fff9286dd360db325abc46e65e52812
Mheeh <dreekus@gmail.com>
2016-04-17 02:04:59 +0200

    Rysowanie tego co jest w audiocity. [WIP]

---
+++ amplitude.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#coding: utf-8
+# using python 2.7 Anaconda Interpreter
+# Authors: Ariel Antonowicz, Marcin Lipa, Dariusz Urbański
+
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
+
+def plotGraph(filename,graphpath=None):
+
+    rate,data = wav.read(filename)
+    file = wave.open(filename, 'rb')
+    nchannels, sampwidth, framerate, nframes = file.getparams()[0:4]
+    data = data.T
+    time=[]
+    for i in range(0,nframes):
+        time.append(float(i)*(1./44100.))
+
+    last = time[nframes-1]
+    print "Channels: ",nchannels
+    print "sampleWidth: ",sampwidth
+    print "framerate: ",framerate
+    print "nframes: ",nframes
+
+
+
+    frames=[]
+    for i in range(0,nchannels):
+        frames.append(file.readframes(nframes=1))
+
+    xSize = 15
+    ySize = 6
+    plt.figure(figsize=(xSize, ySize*nchannels))
+    for i in range(0,nchannels):
+        plt.subplot(211+i)
+        plt.title('Channel '+str(i+1))
+        plt.xlim(0,last)
+        if nchannels > 1:
+            plt.plot(time,data[i])
+        else:
+            plt.plot(time,data)
+        plt.xlabel('Time [s]')
+
+    print "Done"
+
+    if graphpath is None:
+        plt.show()
+    else:
+        plt.savefig(filename,graphpath)
+
+plotGraph(filename="./static/music/temp.wav")
